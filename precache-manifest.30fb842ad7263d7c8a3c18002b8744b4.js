417de10ee03e2b5d25d2dfc9a0774ee0e45c807d
ra.zinurov <ra.zinurov@tensor.ru>
2019-04-04 19:36:52 +0300

    Updates

---
+++ precache-manifest.30fb842ad7263d7c8a3c18002b8744b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c749c5fbbb83e590263b8832a3ec1ef1",
+    "url": "/static/media/SuisseIntl-Book-WebM.c749c5fb.svg"
+  },
+  {
+    "revision": "c20b7d1194bedb79d9c3d4e2d55941e3",
+    "url": "/static/media/SuisseIntl-Book-WebM.c20b7d11.woff"
+  },
+  {
+    "revision": "5f777e79ca58a3d3ae6f61d6dbea10af",
+    "url": "/static/media/SuisseIntl-Book-WebM.5f777e79.ttf"
+  },
+  {
+    "revision": "0c6a404fd1628889d2b0f8206e2f3b37",
+    "url": "/static/media/SuisseIntl-Book-WebM.0c6a404f.eot"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ef72c2517427d268825",
+    "url": "/static/js/main.e635000d.chunk.js"
+  },
+  {
+    "revision": "517a514c37c7c373bf55",
+    "url": "/static/js/2.befa1870.chunk.js"
+  },
+  {
+    "revision": "8ef72c2517427d268825",
+    "url": "/static/css/main.551ccf79.chunk.css"
+  },
+  {
+    "revision": "255acd8524fadc9b0612f8e1e8d2aca2",
+    "url": "/index.html"
+  }
+];
