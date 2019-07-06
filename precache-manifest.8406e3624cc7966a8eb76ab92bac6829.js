2b079240312527f6eedd321405ca7175add41516
ra.zinurov <razinurov@gmail.com>
2019-07-06 23:32:22 +0300

    Updates

---
+++ precache-manifest.8406e3624cc7966a8eb76ab92bac6829.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d94ca1bf2b5a9647f501d1344eacdf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eedea0637ec6baefc1b",
+    "url": "/static/css/main.f6bfe163.chunk.css"
+  },
+  {
+    "revision": "6e34e564391e2b0c4fa2",
+    "url": "/static/js/2.c38b76f0.chunk.js"
+  },
+  {
+    "revision": "0eedea0637ec6baefc1b",
+    "url": "/static/js/main.14121bbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c6a404fd1628889d2b0f8206e2f3b37",
+    "url": "/static/media/SuisseIntl-Book-WebM.0c6a404f.eot"
+  },
+  {
+    "revision": "5f777e79ca58a3d3ae6f61d6dbea10af",
+    "url": "/static/media/SuisseIntl-Book-WebM.5f777e79.ttf"
+  },
+  {
+    "revision": "904a092428345cb6615eaf57c2a31e86",
+    "url": "/static/media/SuisseIntl-Book-WebM.904a0924.svg"
+  },
+  {
+    "revision": "c20b7d1194bedb79d9c3d4e2d55941e3",
+    "url": "/static/media/SuisseIntl-Book-WebM.c20b7d11.woff"
+  }
+]);
